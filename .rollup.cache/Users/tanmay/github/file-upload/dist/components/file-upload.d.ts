624b22c9735f27e42e05f2d8f4a9fffece729af8
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-07-31 03:43:34 +0530

    removed rollup cache

--- .rollup.cache/Users/tanmay/github/file-upload/dist/components/file-upload.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import type { FileUploadProps } from "../types";
-/**
- * Main FileUpload component with variant support
- */
-export declare const FileUpload: React.ForwardRefExoticComponent<FileUploadProps & React.RefAttributes<HTMLDivElement>>;
-//# sourceMappingURL=file-upload.d.ts.map
